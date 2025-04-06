5fd697bef2df73e2d83f3aaad33fdafa00c5c3d2
Christian Ribeiro <christian.des.ribeiro@gmail.com>
2025-04-06 11:57:18 -0300

    refact(RefitExtension) include ServiceProvider

--- src/Core/RefitExample.ApiClient/Refit/Extensions/MicroserviceRefitExtension.cs
+++ src/Core/RefitExample.ApiClient/Refit/Extensions/MicroserviceRefitExtension.cs
@@ -6,7 +6,7 @@ namespace RefitExample.ApiClient.Refit.Extensions;
 
 public static class RefitServiceCollectionExtensions
 {
-    public static IServiceCollection AppendRefitInterfaces<TTypeInterface>(this IServiceCollection services, Action<HttpClient, Type> httpClientConfigurator, Func<IHttpClientBuilder, IHttpClientBuilder>? configureHttpClientBuilder = null)
+    public static IServiceCollection AppendRefitInterfaces<TTypeInterface>(this IServiceCollection services, Action<HttpClient, Type, IServiceProvider> httpClientConfigurator, Func<IHttpClientBuilder, IHttpClientBuilder>? configureHttpClientBuilder = null)
         where TTypeInterface : IBaseRefitInterface
     {
         var types = GetListInterfaceType(typeof(TTypeInterface));
@@ -15,7 +15,7 @@ public static class RefitServiceCollectionExtensions
         {
             var httpClientBuilder = services
                 .AddRefitClient(item)
-                .ConfigureHttpClient(client => httpClientConfigurator(client, item));
+                .ConfigureHttpClient((serviceProvider, client) => httpClientConfigurator(client, item, serviceProvider));
 
             configureHttpClientBuilder?.Invoke(httpClientBuilder);
         }
@@ -23,10 +23,16 @@ public static class RefitServiceCollectionExtensions
         return services;
     }
 
+    public static IServiceCollection AppendRefitInterfaces<TTypeInterface>(this IServiceCollection services, Action<HttpClient, Type> httpClientConfigurator, Func<IHttpClientBuilder, IHttpClientBuilder>? configureHttpClientBuilder = null)
+        where TTypeInterface : IBaseRefitInterface
+    {
+        return services.AppendRefitInterfaces<TTypeInterface>((client, type, _) => httpClientConfigurator(client, type), configureHttpClientBuilder);
+    }
+
     public static IServiceCollection AppendRefitInterfaces<TTypeInterface>(this IServiceCollection services, Action<HttpClient> httpClientConfigurator, Func<IHttpClientBuilder, IHttpClientBuilder>? configureHttpClientBuilder = null)
         where TTypeInterface : IBaseRefitInterface
     {
-        return services.AppendRefitInterfaces<TTypeInterface>((client, _) => httpClientConfigurator(client), configureHttpClientBuilder);
+        return services.AppendRefitInterfaces<TTypeInterface>((client, _, _) => httpClientConfigurator(client), configureHttpClientBuilder);
     }
 
     private static IEnumerable<Type> GetListInterfaceType(Type baseInterface)
