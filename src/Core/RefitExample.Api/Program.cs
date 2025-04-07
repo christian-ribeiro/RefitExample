1c049a67bc1e814606d38bede1e33a6a183f3890
Christian Ribeiro <christian.ribeiro@harmonit.com.br>
2025-04-07 09:38:55 -0300

    doc(ValidateRefit) Include validate - MicroserviceRefit Required

--- src/Core/RefitExample.Api/Program.cs
+++ src/Core/RefitExample.Api/Program.cs
@@ -21,6 +21,14 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IUserService, UserService>();
 
 #region Refit
+//Tratativa para garantir que nenhum Refit que herde de IMicroserviceRefitInterface esteja sem o DataAnnotation
+var missing = typeof(IBaseRefitInterface).Assembly.GetTypes()
+    .Where(t => typeof(IMicroserviceRefitInterface).IsAssignableFrom(t) && t.IsInterface && t != typeof(IMicroserviceRefitInterface))
+    .Where(x => x.GetCustomAttribute<MicroserviceRefitAttribute>() == null).ToList();
+
+if (missing.Count != 0)
+    throw new InvalidOperationException($"As seguintes interfaces estão sem o atributo MicroserviceName: {string.Join(", ", missing.Select(m => m.Name))}");
+
 var refitSettings = new RefitSettings
 {
     ContentSerializer = new SystemTextJsonContentSerializer(new JsonSerializerOptions
